3e034e8b5e03e19918120d1816b3238e52de78c5
SujanSp27 <poojarysujan77@gmail.com>
2025-02-17 21:58:06 +0530

    pgm5.c

--- pgm5.c
+++ pgm5.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    // List of assignments and exams with their scores
     struct Grade {
         char name[50];
         int score;
     };
-
-    // Create an array of grade structures
     struct Grade gradebook[] = {
         {"Assignment 1", 80},
         {"Assignment 2", 90},
@@ -16,22 +13,15 @@ int main() {
 
     int total_score = 0;
     int count = sizeof(gradebook) / sizeof(gradebook[0]);
-
-    // Loop to calculate total score
     for (int i = 0; i < count; i++) {
         total_score += gradebook[i].score;
     }
-
-    // Calculate the average grade
     float average_grade = (float)total_score / count;
 
-    // Display gradebook details
     printf("Gradebook Details:\n");
     for (int i = 0; i < count; i++) {
         printf("%s: %d\n", gradebook[i].name, gradebook[i].score);
     }
-
-    // Display total score and average grade
     printf("\nTotal Score: %d\n", total_score);
     printf("Average Grade: %.2f\n", average_grade);
 
